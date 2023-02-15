193884d043f9027194808a252da10b2ddae91ec4
Wassy | Ryoga Washizawa <ryg.wsza@gmail.com>
2023-02-15 11:55:27 +0000

    Add: 2-wheel travel function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "mbed.h"
 
-#define max_speed 0.4
+#define max_speed 1.0
+// #define max_rotate 0.05
 
 RawSerial pc(USBTX, USBRX, 9600);
-RawSerial sbdbt(A0, A1, 2400);
+RawSerial sbdbt(PA_0, PA_1, 2400);
 
-DigitalInOut chassis[3] = {D2, D7, D4}; // r, arm, l
-PwmOut chassis_p[3] = {D6, D5, D9}; // r, arm, l
+DigitalOut chassis[3] = {PA_10, PA_8, PB_5};    // r, arm, l
+PwmOut chassis_p[3] = {PB_10, PB_4, PC_7};    // r, arm, l
 
 int PS3[7];
+// int flag_command;
+// int counter;
 
 void PS3_data() {
     int sbdbt_data = sbdbt.getc();
@@ -36,69 +39,86 @@ void config() {
 void init_pwm() {
     chassis_p[0].period_us(100);
     chassis_p[1].period_us(100);
+    chassis_p[2].period_us(100);
 
     pc.printf("PWM initialization is complete.");
-    wait_ms(200);
+    wait_us(200000);
 }
 
-void move_pwm(int status_chassis) {
+void move_pwm(double status_chassis) {
     chassis_p[0] = status_chassis * max_speed;
-    chassis_p[1] = status_chassis * max_speed;
+    //chassis_p[1] = status_chassis * max_speed;
+    chassis_p[2] = status_chassis * max_speed;
     
-    pc.printf("%x\tstatus = %d\r\n", PS3[2], status_chassis);
+    pc.printf("%x\tstatus = %lf\r\n", PS3[2], status_chassis);
 }
 
-void init_robot() {
+// void init_arm() {
+//     chassis[1] = 0.0;
+//     wait_us(100000);
+
+//     chassis[1].period_ms(12);
+
+//     flag_command = 0;
+//     counter = 0;
+
+//     pc.printf("Arm initalization is complete.\r\n");
+//     wait_us(100000);
+// }
+
+void setup() {
     config();
 
     pc.printf("Enter both sticks below.\r\n");
     do {
-        wait_ms(1);
+        wait_us(1000);
     } while((PS3[4] != 0x7F) && (PS3[6] != 0x7F));
 
     init_pwm();
 
-    pc.printf("Robot initialization is complete.\r\n\r\n\r\n\r\n");
-    wait_ms(500);
+    pc.printf("Robot initialization is complete.\r\n");
+    wait_us(500000);
+
     pc.printf("To exit, press the '〇' button.\r\n");
     do {
-        wait_ms(1);
+        wait_us(1000);
     } while(PS3[2] != 0x40);
-    wait_ms(1000);
+    wait_us(1000000);
 }
 
 void move_chassis() {
     switch(PS3[2]) {
         case 0x01:
             chassis[0] = 0;
-            chassis[1] = 1;
+            chassis[2] = 1;
             pc.printf("Robot is moving forward.\r\n");
-            move_pwm(1);
+            move_pwm(0.5);
             break;
 
         case 0x02:
             chassis[0] = 1;
-            chassis[1] = 0;
+            chassis[2] = 0;
             pc.printf("Robot is moving Back.\r\n");
-            move_pwm(1);
+            move_pwm(0.5);
             break;
 
         default:
             pc.printf("%d\r\n", PS3[1]);
 
+
         if(PS3[2] == 0x10) {
             switch(PS3[1]) {
                 case 0x04:
                     chassis[0] = 0;
-                    chassis[1] = 0;
-                    move_pwm(1);
+                    chassis[2] = 0;
+                    move_pwm(0.5);
                     pc.printf("Robot is rotating Left.\r\n");
                     break;
 
                 case 0x10:
                     chassis[0] = 1;
-                    chassis[1] = 1;
-                    move_pwm(1);
+                    chassis[2] = 1;
+                    move_pwm(0.5);
                     pc.printf("Robot is rotating Right.\r\n");
                     break;
 
@@ -110,8 +130,34 @@ void move_chassis() {
     }
 }
 
-int main() {
-    pc.printf("Writing Completed.\r\n");
+// void move_arm() {
+//     chassis[1] = 0;
 
-    init_robot();
+//     switch(PS3[2]){
+//         //△＋上キーの入力: 上
+//         case 0x11:
+//             chassis[1] = 0;
+//             chassis[1] = max_rotate * 0.4;
+//             pc.printf("UNIT_C is rotating to the Up.\r\n");
+//             break;
+//         //△＋下キーの入力: 下
+//         case 0x12:
+//             chassis[1] = 1;
+//             chassis[1] = max_rotate;
+//             pc.printf("UNIT_C is rotating to the Down.\r\n");
+//             break;
+//         default:
+//             chassis[1] = 0;
+//     }
+// }
+
+int main() {
+    pc.printf("Writing completed.\r\n");
+
+    setup();
+
+    while(1) {
+        move_chassis();
+        // move_arm();
+    }
 }
